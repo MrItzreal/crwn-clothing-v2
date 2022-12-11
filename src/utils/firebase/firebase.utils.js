eebaa53433747f4853beaf6c51aeedcf8f9ad15d
Israel <israel4321@hotmail.com>
2022-12-10 21:47:28 -0500

    created the first half of the sign up form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc, //retrieves the data
+  getDoc, //access the data
+  setDoc, //sets the data
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,15 +26,19 @@ const firebaseConfig = {
 const firebaseapp = initializeApp(firebaseConfig);
 //In order to use GoogleAuthProvider we need
 //to initialize a provider using this code below
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
   prompt: "select_account",
   //prompt: "select_account",this means every time someone interacts
   //with our provider they will be forced to create an account
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 //export const db = getFirestore() allows us to tell firebase
@@ -37,6 +46,7 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
+  //uid stands for unique id identifier
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -44,6 +54,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   //false if it doesn't, true, if it does
 
   if (!userSnapshot.exists()) {
+    //We check if the snapshot exists
+    //If it doesn't then we set it up with the code below
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
